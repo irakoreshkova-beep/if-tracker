b28367ab0c4051e898a77c9df96f5a36be30c7c8
irakoreshkova-beep <irakoreshkova@gmail.com>
2026-06-01 01:05:58 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'if-tracker-v14';
+const CACHE = 'if-tracker-v16';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('message', function(e) {
